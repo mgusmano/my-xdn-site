5c59c6ce73d8f3c7352d63cb964f5b675588f92d
mgusmano <mgusmano@yahoo.com>
2020-05-25 11:52:18 -0400

    initial code

---
+++ xdn.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: './src/routes.ts',
+  backends: {
+    origin: {
+      domainOrIp: 'mgusmano.myshopify.com',
+      hostHeader: 'mgusmano.myshopify.com',
+    },
+  },
+}
